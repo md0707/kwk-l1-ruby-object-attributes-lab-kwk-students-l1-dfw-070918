84dab8ccd78614fdacc12bea88e6cdaa2554af56
md0707 <ppuggy1@gmail.com>
2018-07-13 18:28:32 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,4 +4,19 @@ class Person
     @job = job
   end
   
-  def 
+  def name
+    name = @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def job
+    job = @job
+  end
+  
+  def job=(job)
+    @job = job
+  end
+end
