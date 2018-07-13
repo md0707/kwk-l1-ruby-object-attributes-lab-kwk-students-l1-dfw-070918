26aa742ea95422e432b630af461bc9909ac9f9db
md0707 <ppuggy1@gmail.com>
2018-07-13 18:29:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# dog.rb
+class Dog
+  def initalize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    name = @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def breed
+    breed = @breed
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
-# person.rb
+class Person
+  def initalize(name, job)
+    @name = name
+    @job = job
+  end
+  
+  def name
+    name = @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def job
+    job = @job
+  end
+  
+  def job=(job)
+    @job = job
+  end
+end
